d7d1769283d5bef4d6acf0fcb3ea5d78dc7ba7c9
multiiscream <multiiscream@gmail.com>
2025-11-23 21:31:53 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def send_message():
 
     # 3. 보낼 메시지 내용 (원하는 대로 수정하세요)
     # \n은 '줄바꿈'입니다.
-    message_text = f"Lv: {d_day:,} / 36,525 ({round(d_day/36525)} %)\n" \
+    message_text = f"Lv: {d_day:,} / 36,525 ({round(d_day*100/36525)} %)\n" \
                    f"Todays left : {2097 - today.year:,}\n" \
 
     # 4. 텔레그램으로 전송
